f4fdbfdc82fdf3c0dc6c277dca264e3421734883
Aditya Parulekar <adityaup@cs.utexas.edu>
2023-05-17 04:32:02 -0500

    more pics

--- labml_nn/diffusion/stable_diffusion/step_script.sh
+++ labml_nn/diffusion/stable_diffusion/step_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Define the step sizes to use
-STEPS=(0.7 0.49 0.39 0.32 0.27 0.23 0.2 0.18 0.16 0.148)
+STEPS=(0.49 0.23 0.147 0.077 0.0395 0.01602 0.00805)
 
+mkdir ourOuts
+cd ourOuts
 # Loop over the step sizes and run the command
 for STEP in "${STEPS[@]}"
 do
     mkdir $STEP
     cd $STEP
-    python3 ../scripts/text_to_image.py --sampler ddpm --step_size_eps "$STEP" --prompt "a saxophone player on a trampoline"
+    python3 ../../scripts/text_to_image.py --sampler ddpm --step_size_eps "$STEP" --prompt "a squirrel swimming freestyle in a swimming pool" --batch_size 2
     cd ..
 done
